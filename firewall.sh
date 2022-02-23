9c4c80bd7ea14b725fa2878aa1f1c80c064549a4
Hugin Kunoy Knudsen <huginkk@gmail.com>
2022-02-23 11:33:29 +0000

    commit

--- firewall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#ansible-playbook -i inventory.yml playbooks/firewall.yml -l all -vvv
-ansible-playbook -i inventory.yml playbooks/firewall.yml -l haproxies,terra_nodes,grafana_servers,prometheus_servers -vvv
--- terra-nodes.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ansible-playbook -i inventory.yml playbooks/terra-nodes.yml -l terra_nodes -vvv
